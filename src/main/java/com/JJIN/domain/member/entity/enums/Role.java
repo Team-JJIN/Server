026625d7102d1252d00f7c0275c8fcaaf5985110
seahee <lollol0617@gmail.com>
2026-07-13 11:26:34 +0900

    init: Role enum 추가

---
+++ src/main/java/com/JJIN/domain/member/entity/enums/Role.java
@@ -0,0 +1,23 @@
+package com.JJIN.domain.member.entity.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+	MEMBER("ROLE_MEMBER"),
+	ADMIN("ROLE_ADMIN"),
+	;
+
+	private final String roleName;
+
+	public static Role fromRoleName(String roleName) {
+		for (Role role : values()) {
+			if (role.roleName.equals(roleName)) {
+				return role;
+			}
+		}
+		return null;
+	}
+}
