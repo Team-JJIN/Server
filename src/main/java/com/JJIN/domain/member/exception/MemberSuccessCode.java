9a8246261a479a558c764e100e4da312586e0a23
seahee <lollol0617@gmail.com>
2026-07-20 20:33:43 +0900

    feat: MemberErrorCode, MemberSuccessCode 작성

--- src/main/java/com/JJIN/domain/member/exception/MemberSuccessCode.java
+++ src/main/java/com/JJIN/domain/member/exception/MemberSuccessCode.java
@@ -18,6 +18,9 @@ public enum MemberSuccessCode implements BaseCode {
 	REISSUE_SUCCESS(HttpStatus.OK, "액세스 토큰 재발급에 성공했습니다."),
 	LOGOUT_SUCCESS(HttpStatus.OK, "로그아웃에 성공했습니다."),
 	ROLE_CHANGE_SUCCESS(HttpStatus.OK, "role 변경에 성공했습니다."),
+	EMAIL_CODE_SENT(HttpStatus.OK, "인증코드를 발송했습니다."),
+	EMAIL_VERIFIED(HttpStatus.OK, "이메일 인증에 성공했습니다."),
+	TERMS_FETCH_SUCCESS(HttpStatus.OK, "약관 목록 조회에 성공했습니다."),
 
 	/*
 	201 CREATED
